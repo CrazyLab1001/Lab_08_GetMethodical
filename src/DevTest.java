7f7c175dbcfb8e4931cf0c7e3e76e634e37d3664
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-03-07 10:11:02 -0500

    Troubleshooting getRangedInt

--- src/DevTest.java
+++ src/DevTest.java
@@ -10,7 +10,30 @@ public class DevTest {
         randoNum = SafeInput.getInt(in, "Give me a random number! ");
         double randoDouble = 0.00;
         new Scanner(System.in);
-        randoDouble = SafeInput.getDouble(in, "Now give me a number with a few decimals! ");
-
+        randoDouble = SafeInput.getDouble(in, "Now give me a number with a decimal! ");
+        int rangedInt = 0;
+        new Scanner(System.in);
+        rangedInt = getRangedInt(in, "Now I need a ranged number, 1 through 12! ", 1, 12);
     }
+
+    private static int getRangedInt(Scanner pipe, String prompt, int low, int high) {
+        int result = 0;
+        boolean done = false;
+        String trash = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+        if (pipe.hasNextInt())
+        {
+            result = pipe.nextInt();
+            pipe.nextLine();
+            if (result >= low && result <= high) {
+                done = true;
+            } else {
+                System.out.println("Please put in the ranged input unlike the value: " + result );
+            }
+        } else
+            trash = pipe.nextLine();
+            System.out.println("Please insert a valid input unlike: " + trash );
+        } while (!done);
+    return result; }
 }
