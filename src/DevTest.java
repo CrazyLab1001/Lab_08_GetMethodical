00f8678f11495d26ea4053f94eb3a4cab3d6246d
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-03-07 11:27:35 -0500

    getYNConfirm is made

--- src/DevTest.java
+++ src/DevTest.java
@@ -17,5 +17,32 @@ public class DevTest {
         double rangedDouble = 0;
         new Scanner(System.in);
         rangedDouble = SafeInput.getRangedDouble(in, "Now a ranged number between 0.5 and 32.84! ", .5, 32.84);
+        boolean forTrueYforFalseN;
+        new Scanner(System.in);
+        do {
+        forTrueYforFalseN = getYNConfirm(in, "Do you want to stop with this question?"); } while (!forTrueYforFalseN);
+    }
+    private static boolean getYNConfirm(Scanner pipe, String prompt) {
+        boolean result = false;
+        String YNOption = "";
+        boolean done = false;
+        String trash = "";
+        do {
+            System.out.print("\n" + prompt + ": ");
+            YNOption = pipe.nextLine();
+            switch (YNOption) {
+                case "Y":
+                    result = true;
+                    done = true;
+                    break;
+                case "N":
+                    result = false;
+                    break;
+                default:
+                    trash = YNOption;
+                    System.out.println("Please insert a valid input unlike: " + trash );
+            }
+        } while (!done);
+        return result;
     }
 }
